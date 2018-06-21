dc6dae486a528cbc163d9e6b0dab60bac7ba0bcf
nckunal <nck12343@gmail.com>
2018-06-21 17:52:04 +0530

    First commit

---
+++ js/jscode.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+  $("#color").on("click" , function(){
+    $(".changeText").val("Red");
+  });
+  $("#color2").on("click" , function(){
+    $(".changeText").val("Green");
+  });
+  $("#color3").on("click" , function(){
+    $(".changeText").val("Blue");
+  });
+});
+
+
